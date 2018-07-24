8fbc6ce86408e552b71e2aaf2e23bfc1ac005b84
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-07-23 17:46:39 -0700

    initial server code

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send({
+    test: 'string'
+  })
+});
+
+app.listen(PORT);
+
+console.log(`Listening on ${PORT}`);
