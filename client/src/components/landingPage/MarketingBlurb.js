c25ce789593ee0cffd31cd5963b0e8700c2ca182
Jenny <jenny@synapsestudios.com>
2018-08-19 23:23:34 -0700

    Added favicon and front page image

--- client/src/components/landingPage/MarketingBlurb.js
+++ client/src/components/landingPage/MarketingBlurb.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import '../styles/style.css'
 import '../styles/main.css'
+import DiceImage from '../Images/DiceImage.png';
 
 const MarketingBlurb = () => {
     return(
@@ -13,6 +14,10 @@ const MarketingBlurb = () => {
                 <h3 className = "boxTitle">Welcome to Battlerounds!</h3>
             </div>
             <div className="box" id="roundbox">
+                <div class="ContentImage">
+                <img src={DiceImage} style={{width: '30%', float: 'right'}}/>
+                </div>
+
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet neque quis urna molestie euismod. Aliquam tristique egestas lacus, sed tincidunt velit pretium id. Suspendisse semper turpis quis diam malesuada porta. Sed rutrum, magna id condimentum tincidunt, velit purus malesuada massa, at rutrum ipsum nunc eu est. Fusce ac consequat turpis, eu lobortis lectus. In varius risus eget elementum mollis.</p>
                 <p />
                 <p>Donec iaculis quam a laoreet vestibulum. Fusce ac vehicula urna. Phasellus ullamcorper eget lacus eu suscipit. Curabitur nec urna rhoncus, luctus dolor at, lobortis sapien. Phasellus sed ultricies tortor, quis tristique lectus. Praesent quis purus nisl. Ut facilisis at mi ac dictum. Ut ac purus dolor.</p>
