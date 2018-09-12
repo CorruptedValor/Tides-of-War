f3fdae336396b4c3fd9d89e37cae05867e90c9c2
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-09-11 21:18:40 -0700

    admin loader

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -11,26 +11,31 @@ import MatchForm from './MatchForm';
 class Admin extends Component {
     componentWillMount() {
         this.props.fetchUser();
+        this.props.fetchAllPlayers();
     }
     
     renderContent() {
         const {user} = this.props;
         
         if( user === null) {
-            return <AdminReqError />;
+            return <p>loading...</p>; //loader
 
         } else if (!user.auth) {
 
             return <AdminReqError />;
 
         } else if(user.auth && user.auth.isAdmin){
-            return [
-                <div key ="10">
-                    <PlayerForm key="1" />
-                    <MatchForm key="2" />
-                </div>
+            if (!this.props.playerList.data){
+                //loader
+                return <p>loading...</p>
+            } else {
+                return [
+                    <div key ="10">
+                        <PlayerForm key="1" />
+                        <MatchForm key="2" />
+                    </div>
                 ];
-
+            }
         } else 
             return <AdminReqError />;
         
@@ -47,14 +52,16 @@ class Admin extends Component {
 
 function mapStateToProps(state) {
     return {
-        user: state.user
+        user: state.user,
+        playerList: state.playerList
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchUser: () => dispatch(actions.fetchUser())
-    }  ;      
+        fetchUser: () => dispatch(actions.fetchUser()),
+        fetchAllPlayers: () => dispatch(actions.fetchAllPlayers())
+    };      
 }
 
 
