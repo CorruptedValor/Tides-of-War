25087a1226ef12950c439724fffe51808a457a89
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-09-12 15:50:23 -0700

    more news changes

--- client/src/components/newsPage/News.js
+++ client/src/components/newsPage/News.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import '../styles/main.css';
 import '../styles/style.css';
 import NewsCards from './NewsCards';
 import Pagination from './Pagination';
 
-const News = () => {
-  return(
-		<div className="boxContainer">
-			<div>
-				<div className = "boxHeader" id="grad">
-					<h3 className = "boxTitle">News</h3>
-				</div>
-				<div className="box" id="roundbox">
-				<NewsCards/>
-				<Pagination/>
+class News extends Component {
+  render(){
+
+		return(
+			<div className="boxContainer">
+				<div>
+					<div className = "boxHeader" id="grad">
+						<h3 className = "boxTitle">News</h3>
+					</div>
+					<div className="box" id="roundbox">
+					<NewsCards/>
+					<Pagination/>
+					</div>
 				</div>
 			</div>
-		</div>
-    );
-
+		);
+	}
 };
 
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+        playerList: state.playerList
+    };
+}
 
-export default News;
+const mapDispatchToProps = (dispatch) => {
+    return {
+		
+        fetchUser: () => dispatch(actions.fetchUser()),
+        fetchAllPlayers: () => dispatch(actions.fetchAllPlayers())
+
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(News);
